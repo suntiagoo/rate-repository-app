e9454ed80bcbf7b431fd12c24b0bfabfad283599
Santiago Sandoval <191775772+suntiagoo@users.noreply.github.com>
2026-02-23 09:21:08 +0200

    Exercises 10.1 - 10.2

---
+++ rate-repository-app/eslint.config.mjs
@@ -0,0 +1,45 @@
+import { defineConfig } from "eslint/config";
+import react from "eslint-plugin-react";
+import globals from "globals";
+import babelParser from "babel-eslint";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  {
+    extends: compat.extends("eslint:recommended", "plugin:react/recommended"),
+
+    plugins: {
+      react,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: babelParser,
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "react/prop-types": "off",
+      semi: "error",
+    },
+  },
+]);
